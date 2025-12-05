ae3362916743a6366699fa3a4e1531c19910c457
enrique <enrique.darderes@gmail.com>
2025-12-06 00:26:37 +0100

    medieval

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -54,7 +54,7 @@ function AlertDialogContent({
       <AlertDialogPrimitive.Content
         data-slot="alert-dialog-content"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:max-w-lg",
+          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 border-2 border-double p-6 shadow-lg duration-200 sm:max-w-lg rounded-none [clip-path:polygon(0%_0%,100%_0%,100%_10%,98%_20%,100%_30%,100%_70%,98%_80%,100%_90%,100%_100%,0%_100%,0%_90%,2%_80%,0%_70%,0%_30%,2%_20%,0%_10%)]",
           className
         )}
         {...props}
