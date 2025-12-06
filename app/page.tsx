e379228337e7e21a1b117c55b4ec834d179e0483
enrique <enrique.darderes@gmail.com>
2025-12-06 09:32:24 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,18 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button>Primary button</Button>
-        <Button variant="secondary">Secondary button</Button>
-        <Button variant="outline">Outline button</Button>
-        <Button variant="ghost">Ghost button</Button>
+        <div className="flex gap-8">
+          <div className="flex flex-col gap-2">
+            <Button>Primary button</Button>
+            <Button variant="secondary">Secondary button</Button>
+            <Button variant="outline">Outline button</Button>
+            <Button variant="ghost">Ghost button</Button>
+          </div>
 
-        <CardDemo />
-        <AlertDialogDemo />
+          <CardDemo />
+          <AlertDialogDemo />
+        </div>
+
 
       </main>
 
